5e45693c3c016cd147b28361934b5829bae294d5
parth782 <parth11gupta@gmail.com>
2022-12-18 19:12:35 +0530

    bug fixing

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const connecttomongo=require('./db');
 const express = require('express')
 var cors = require('cors')
-
-
-
 connecttomongo();
 const app = express()
-
-
 app.use(cors())
 app.use(express.json())
 
@@ -16,14 +11,6 @@ app.get('/', (req, res) => {
 })
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
-app.use('/api/projects')
-// app.get('/api/v1/login', (req, res) => {
-//     res.send('Hello login!')
-//   })
-//   app.get('/api/v1/signup', (req, res) => {
-//     res.send('Hello Signup!')
-//   })
-
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Example app listening at http://localhost:5000`)
 })
